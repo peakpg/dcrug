6a586a5b96f9757dc43df851c8dcd5dce528a874
Patrick Peak <peakpg@gmail.com>
2014-03-11 15:17:29 -0400

    Add most recent posts portlet
    * Used to list posts in right sidebar.

--- app/models/dcrug/post.rb
+++ app/models/dcrug/post.rb
@@ -3,5 +3,10 @@ module Dcrug
     acts_as_content_block
     is_addressable path: "/posts", template: "subpage"
     content_module :dcrug
+
+    def self.most_recent_first
+      order('release_date desc')
+    end
+
   end
 end
--- app/portlets/recent_posts_portlet.rb
+++ app/portlets/recent_posts_portlet.rb
@@ -1,12 +1,12 @@
-class PostsPortlet < Cms::Portlet
+class RecentPostsPortlet < Cms::Portlet
 
-  description "Lists all Posts for the homepage."
+  description "Simple list of recent posts"
 
   # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
   enable_template_editor false
      
   def render
-    @posts = Dcrug::Post.most_recent_first
+    @posts = Dcrug::Post.limit(self.max_posts).most_recent_first
   end
     
 end
